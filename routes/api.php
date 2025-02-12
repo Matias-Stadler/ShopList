8aa0e317c307ba0dd15ebc049033568b29bc0bcb
matias-stadler <koutarcj@gmail.com>
2025-02-12 09:07:49 +0100

    fix:add routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/products',[ProductController::class,'index'])->name('apihome');
+Route::delete('/products/{id}',[ProductController::class,'destroy'])->name('apidestroy');
+Route::post('/products', [ProductController::class,'store'])->name('apistore');
+Route::put('/products/{id}',[ProductController::class, 'update'])->name('apiupdate');
+Route::delete('/products',[ProductController::class, 'destroyAll'])->name('apidestroyall');
